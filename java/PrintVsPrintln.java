3b3eb197431e381a95b7f24c9742bc01c6e557b8
tjadams <tyler@virtualpowersystems.com>
2015-12-10 21:39:13 -0800

    Further understand this example

--- java/PrintVsPrintln.java
+++ java/PrintVsPrintln.java
@@ -5,9 +5,17 @@ public class PrintVsPrintln {
         // Line 2: I wonder if someone will ever read this repository"
         // Turns out it doesn't.
         System.out.println("Line 1:");
-        System.out.print(" Not appended to Line 1, but rather prepended to Line 2. ");
+        System.out.print("Not appended to Line 1, but rather prepended to Line 2.");
         System.out.println("Line 2: I wonder if someone will ever read this repository");
 
+        // Here's an example of 3 prints, a newline, and a print
+        System.out.println();
+        System.out.print("1");
+        System.out.print("2");
+        System.out.println("3");
+        System.out.print("4");
+
+        System.out.println();
         System.out.print("So it's time to evolve");
         // Note: don't use \n because that is specifically a newline character which is some specific unicode char.
         // Instead, use %n with System.out.format whichwill be the correct newline character for your platform.
