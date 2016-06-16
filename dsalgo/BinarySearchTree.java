400330599a58f527a3e28edc0a02c309d1ca310d
Tyler Adams <tjadams@uwaterloo.ca>
2016-06-15 20:07:59 -0700

    Create skeleton for BinarySearchTree class

---
+++ dsalgo/BinarySearchTree.java
@@ -0,0 +1,49 @@
+public class BinarySearchTree {
+    private BinarySearchTree left;
+    private BinarySearchTree right;
+    private int value;
+
+    public int min() {
+        // TODO
+        return 0;
+    }
+
+    public int max() {
+        // TODO
+        return 0;
+    }
+
+    public BinarySearchTree successor() {
+        // TODO
+        return null;
+    }
+
+    public BinarySearchTree predecessor() {
+        // TODO
+        return null;
+    }
+
+    public BinarySearchTree getLeft() {
+        return left;
+    }
+
+    public BinarySearchTree getRight() {
+        return right;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setLeft(BinarySearchTree left) {
+        this.left = left;
+    }
+
+    public void setRight(BinarySearchTree right) {
+        this.right = right;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
